bd0bb738b0ef61cfccac2f614fd70c4f0cd34aef
Pedro Oliveira <pncolvr@gmail.com>
2026-04-18 21:23:10 +0100

    fix: update status only when needed

--- scripts/powermenu.sh
+++ scripts/powermenu.sh
@@ -3,18 +3,32 @@
 function hypr_shutdown() {
     local message="$1"
     local cmd="$2"
+    log_inactive
     hyprshutdown --top-label "$message" --post-cmd "$cmd"
+}
+
+function lock () {
+    log_inactive
+    loginctl lock-session
+}
+
+function logout() {
+    log_inactive
+    hyprshutdown --top-label "Logging out..."
+}
+
+function log_inactive() {
+    $ZDOTDIR/scripts/status/manager.sh --log-system-event inactive
 }
 
 chosen=$(echo -n " Reboot| Lock| Logout| Shutdown| Bios| Cancel" | rofi -sep '|' -dmenu -case-smart -sort -sorting-method fzf -p "")
 
 case $chosen in
     *Reboot*) hypr_shutdown 'Restarting...' 'systemctl reboot';;
-    *Lock*) loginctl lock-session;;
-    *Logout*) hyprshutdown --top-label "Logging out...";;
+    *Lock*) lock;;
+    *Logout*) logout;;
     *Shutdown*) hypr_shutdown 'Shutting down...' 'systemctl poweroff';;
     *Bios*) hypr_shutdown 'Restarting to bios...' 'systemctl reboot --firmware-setup';;
     *) echo "none" && exit 0;;
 esac
 
-$ZDOTDIR/scripts/status/manager.sh --log-system-event shutdown
