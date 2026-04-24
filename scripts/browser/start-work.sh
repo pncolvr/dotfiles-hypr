f854b1ed2c320b4a2934e360a8cd4d59d80adb46
Pedro Oliveira <pncolvr@gmail.com>
2026-04-24 19:11:09 +0100

    refactor: change terminal emulator to ghostty

--- scripts/browser/start-work.sh
+++ scripts/browser/start-work.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-workspaceAlreadyExists=$(hyprctl workspaces | grep "workspace ID 9")
-hyprctl dispatch workspace 9
-if [ -z "$workspaceAlreadyExists" ]; then
-    "$ZDOTDIR"/scripts/default-browser/default-browser.sh https://www.monday.com & disown
-fi
+"$ZDOTDIR"/scripts/default-browser/default-browser.sh https://www.monday.com & disown
--- scripts/toggle/terminal.sh
+++ scripts/toggle/terminal.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
+source $HOME/.config/hypr/scripts/_common/utils.sh
 
-WORKSPACE=$(dirname "$(dirname "${BASH_SOURCE[0]:-0}")")
-
-source "$WORKSPACE"/_common/utils.sh
-
-CLASS="kitty-dropdown"
+CLASS="com.mitchellh.ghostty"
+TITLE="dropdown-term"
 ACTIVE_WORKSPACE=$(hyprctl activeworkspace -j)
 
 # to check window already exists
-WINDOW_ADDRESS=$(hyprctl clients -j | jq -r --arg class_name $CLASS '[.[] | select(.class==$class_name)] | .[0].address // ""')
-if [[ "$WINDOW_ADDRESS" ]]; then
-    hyprctl dispatch closewindow class:$CLASS
+WINDOW_ADDRESS=$(hyprctl clients -j | jq -r --arg class_name $CLASS --arg title $TITLE '[.[] | select(.class==$class_name and .title==$title)] | .[0].address // ""')
+echo $WINDOW_ADDRESS
+if [[ -n "$WINDOW_ADDRESS" ]]; then
+    hyprctl dispatch closewindow address:"$WINDOW_ADDRESS"
 else
-    kitty --class "$CLASS" & disown
+    ghostty --title="$TITLE" & disown
     CURSOR_POSITION=$(calculate_cursor_move_to_position "$ACTIVE_WORKSPACE" "0.5" "0.8")
     hyprctl dispatch movecursor "$CURSOR_POSITION"
 fi
