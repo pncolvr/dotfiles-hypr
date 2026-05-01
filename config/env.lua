9c83d29cb2d62e9198d328678f822276c449ab7a
Pedro Oliveira <pncolvr@gmail.com>
2026-05-01 08:00:07 +0100

    feat: moving config to lua - wip - not tested

---
+++ config/env.lua
@@ -0,0 +1,10 @@
+hl.env("XCURSOR_THEME", "Breeze_Light")
+hl.env("XCURSOR_SIZE", "24")
+
+-- nvidia
+hl.env("LIBVA_DRIVER_NAME", "nvidia")
+hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
+hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
+
+-- path 
+hl.env("PATH", "/bin:/usr/bin:/usr/local/bin:$HOME/.local/bin")
