dce26a03c2c74cf3406ed6a4b5701caa545da67d
Pedro Oliveira <pncolvr@gmail.com>
2026-05-13 10:23:53 +0100

    fix workspaces

--- config/monitors.lua
+++ config/monitors.lua
@@ -3,13 +3,12 @@ hl.monitor({
     output   = "DP-1",
     mode     = "2560x1440@144",
     position = "0x0",
-    scale    = 1,
+    scale    = "1",
 })
 
 -- workspace affinity
 for i = 1, 10 do
-    local key = i % 10
-    hl.workspace_rule({ workspace = tostring(key), monitor = "DP-1", persistent = true })
+    hl.workspace_rule({ workspace = tostring(i), monitor = "DP-1", persistent = true })
 end
 
 
